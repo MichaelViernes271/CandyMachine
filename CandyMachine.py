02d2c1ac7ce2462bb58ffed471b441f4b664cf1a
Michael <michaelviernes.271@outlook.com>
2022-12-10 05:47:16 +0800

    CandyMachine.py fixed in some try-catch blocks.

--- CandyMachine.py
+++ CandyMachine.py
@@ -16,37 +16,37 @@ class CandyMachine:
     def showSelection(self):
         """displays the menu"""
         banner1 = "Welcome to Sweet's Candy Shop".center(50,"*")
-        banner2 =\
-"""
-To select an item enter
-1 for Candy
-2 for Chips
-3 for Gum
-4 for Cookies
-0 to View Balance
-9 to Exit
-"""
+        banner2 = \
+            """
+            To select an item enter
+            1 for Candy
+            2 for Chips
+            3 for Gum
+            4 for Cookies
+            0 to View Balance
+            9 to Exit
+            """
+        print(banner1, banner2)
 
-        while True:
-            # gathering orders from user till it exits
-            # prints display menu
-            print(banner1, banner2)
-
-            try:
-                usr_input = int(input("Enter your choice: "))
-                if ((usr_input > 4 and usr_input < 9) or (usr_input < 0 or usr_input > 9 )):
-                    raise ValueError
-            except:
-                print("Bad choice here. Try again.")
-
-            if (usr_input == 0):
-                print("exiting...")
-                exit()
-
-            if (usr_input == 1):
-                cashchange = self.sellProduct(self.candy, self.cashreg)
-                if not cashchange:
-                    print(f"You bought a cookie. Your change is {cashchange}.\n")
+        # while True:
+        #     # gathering orders from user till it exits
+        #     # prints display menu
+        #
+        #     try:
+        #         usr_input = int(input("Enter your choice: "))
+        #         if ((usr_input > 4 and usr_input < 9) or (usr_input < 0 or usr_input > 9 )):
+        #             raise ValueError
+        #     except:
+        #         print("Bad choice here. Try again.")
+        #
+        #     if (usr_input == 0):
+        #         print("exiting...")
+        #         exit()
+        #
+        #     if (usr_input == 1):
+        #         cashchange = self.sellProduct(self.candy, self.cashreg)
+        #         if not cashchange:
+        #             print(f"You bought a cookie. Your change is {cashchange}.\n")
 
 
     def sellProduct(self, product, cRegister):
@@ -65,7 +65,7 @@ To select an item enter
                     usr_amount = int(input("Enter amount or 0 to cancel: "))
                     if usr_amount == 0:
                         return
-                    assert (usr_amount > product.getProductCost()) or (usr_amount > 0)
+                    assert (usr_amount >= product.getProductCost()) or (usr_amount > 0)
                     break
                 except:
                     print("You must have insufficient money. Try Again.\n")
@@ -76,4 +76,6 @@ To select an item enter
             print(f"Remaining items: {product.getCount()}")
 
             # return cash change if any
-            return usr_amount - product.cost
+            cashchange = usr_amount - product.cost
+            print(f"You bought an item. Your change is {cashchange}.\n")
+            return cashchange
--- main.py
+++ main.py
@@ -10,13 +10,53 @@
 # from Dispenser import Dispenser
 from CandyMachine import CandyMachine
 
+# divided routine
+def mainloop():
+    # gathering orders from user till it exits
+    # prints display menu
+    candymach.showSelection()
+    while True:
+        try:
+            usr_rawinput = input("Enter your choice: ")
+            if usr_rawinput == "" or usr_rawinput == "\n":
+                raise Exception
+            usr_input = int(usr_rawinput)
+            if ((usr_input > 4 and usr_input < 9) or (usr_input < 0 or usr_input > 9)):
+                raise ValueError
+            break
+        except Exception as e:
+            print("Bad choice here. Try again.", e)
+
+    if (usr_input == 0):
+        print("exiting...")
+        exit()
+
+    # for candy item
+    if (usr_input == 1):
+        cashchange = candymach.sellProduct(candymach.candy,candymach.cashreg)
+        # if (not cashchange == 0 and cashchange is not None):
+        #     print(f"You bought a candy. Your change is {cashchange}.\n")
+    # for candy chips
+    if (usr_input == 2):
+        cashchange = candymach.sellProduct(candymach.chips,candymach.cashreg)
+        # # if not (not cashchange == 0 and cashchange is not None):
+        #     print(f"You bought a chips. Your change is {cashchange}.\n")
+    # for gum item
+    if (usr_input == 3):
+        cashchange = candymach.sellProduct(candymach.gum,candymach.cashreg)
+        # if not (not cashchange == 0 and cashchange is not None):
+        #     print(f"You bought a gum. Your change is {cashchange}.\n")
+    # for cooke item
+    if (usr_input == 4):
+        cashchange = candymach.sellProduct(candymach.cookies,candymach.cashreg)
+        # if not (not cashchange == 0 and cashchange is not None):
+        #     print(f"You bought a cookie. Your change is {cashchange}.\n")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     candymach = CandyMachine()
-    candymach.showSelection()
-    pass
-
-
-
+    while True:
+        mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
