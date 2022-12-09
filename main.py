41696e3a420d6619e57e65c2307dec51a5ab35b8
Michael <michaelviernes.271@outlook.com>
2022-12-10 02:38:03 +0800

    template

---
+++ main.py
@@ -0,0 +1,18 @@
+Objective:
+# The program should do the following:
+# 1. Show the customer the different products sold by the candy machine
+# 2. Let the customer make the selection
+# 3. Show the customer the cost of the item selected
+# 4. Accept money from the customer
+# 5. Release the item
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
